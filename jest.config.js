8d36507722366f7fe45c5078d3b0a1f9ec65a5fc
vitoria cabral <vitoria.oliveira@alu.ufc.br>
2023-07-18 17:31:30 -0300

    configuração do jest para tabela de porcentagem dos testes

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  testMatch: ["**/test/**/*.spec.js"], // Padrão para encontrar os arquivos de teste.
+  collectCoverage: true, // Habilita a coleta de cobertura de código.
+  coverageDirectory: "coverage", // Diretório onde os relatórios de cobertura serão gerados.
+};
